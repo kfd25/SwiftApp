5c49aa2ba5cbc000ce97664839e74490ed85cb79
Kevin Fernandes Duarte <>
2018-10-22 22:42:18 +0200

    My first commit

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+
+    var tapCount = 0
+    
+    @IBOutlet weak var editField: UITextField!
+    @IBAction func buttonTapped(_ sender: Any) {
+        theLabel.text = editField.text
+        editField.text = ""
+        
+        tapCount = tapCount + 1
+        print(tapCount)
+        
+        if tapCount > 5 {
+            theLabel.text = "You tapped the Button over 5 times."
+        }
+
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
